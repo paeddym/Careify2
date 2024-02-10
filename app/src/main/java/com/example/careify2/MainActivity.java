75e6e75d0311389a17ab7e43670a5dc81fde3838
paeddym <patrikmarosvary@gmail.com>
2024-02-10 15:54:03 +0100

    First Commit

---
+++ app/src/main/java/com/example/careify2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.careify2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
