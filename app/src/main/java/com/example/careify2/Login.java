d705f6eb2c26e78e18d7dd57da7267d00fdaa7bb
paeddym <patrikmarosvary@gmail.com>
2024-02-10 18:04:17 +0100

    Made Login Button even more functional

--- app/src/main/java/com/example/careify2/Login.java
+++ app/src/main/java/com/example/careify2/Login.java
@@ -2,6 +2,7 @@ package com.example.careify2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,7 @@ public class Login extends AppCompatActivity {
         loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                Toast.makeText(Login.this, "It Works!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Login.this, Category.class));          //MAGIC CODE
             }
         });
     }
