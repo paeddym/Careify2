3d8353bde2821cfe233bda8eb952b4f649668497
paeddym <patrikmarosvary@gmail.com>
2024-02-10 17:52:00 +0100

    Added Button functionality to Login

--- app/src/main/java/com/example/careify2/Login.java
+++ app/src/main/java/com/example/careify2/Login.java
@@ -3,6 +3,9 @@ package com.example.careify2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton;
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Toast.makeText(Login.this, "It Works!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
