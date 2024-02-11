4829e8e08459535ac5eaba3029eee7d551d0d7f4
paeddym <patrikmarosvary@gmail.com>
2024-02-11 11:03:19 +0100

    Added navigation, menu and fixed constraints of AddPatient

--- app/src/main/java/com/example/careify2/AddPatient.java
+++ app/src/main/java/com/example/careify2/AddPatient.java
@@ -2,7 +2,13 @@ package com.example.careify2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class AddPatient extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class AddPatient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_patient);
+        setSupportActionBar(findViewById(R.id.toolbarAddPatient));
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.app_bar_settings) {
+            Snackbar.make(findViewById(android.R.id.content), "To be implemented", Snackbar.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if (id == android.R.id.home) {
+            startActivity(new Intent(AddPatient.this, AllPatients.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
